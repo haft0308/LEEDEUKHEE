684ec60d23eb7bf387f309300857538a361d4e4a
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-22 14:52:51 +0900

    Add files via upload

---
+++ 220322/Gugu_all_2.java
@@ -0,0 +1,19 @@
+
+public class Gugu_all_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("구구단");
+		for (int i=1; i<10; i++)
+		{
+			for (int j=2; j<10; j++)
+			{
+				System.out.printf("%d*%d=%d\t",j,i,j*i);
+			}
+			System.out.println();
+		}
+	}
+
+}

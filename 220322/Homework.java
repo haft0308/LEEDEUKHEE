e91ed8fd61ec08ad5470cd49f85de03bfdc5a1ca
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-22 17:07:45 +0900

    Add files via upload

--- 220322/Homework.java
+++ 220322/Homework.java
@@ -25,15 +25,16 @@ public class Homework {
 			if (a>10 || a<0)
 			{
 				System.out.println("잘못입력하였습니다.");
-				i=i-1; // 카운트에서 제외하기, 카운트 하려면 삭제
+				i=i-1; // // 카운트에서 제외하기, 카운트 하려면 삭제
 			}
 			if (a==0)
 			{
+				i=i-1; // 0 입력을 카운트에서 제외하기 위해 -1
 				break;
 			}
 			
 		}
-		System.out.printf("%d번 입력 받았습니다.",i-1); // 0 입력을 카운트에서 제외하기 위해 -1 함
+		System.out.printf("%d번 입력 받았습니다.",i);
 	}
 
 }

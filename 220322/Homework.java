d16d090d4dbe2f8e3523d7b6657cfdb90355b3ab
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-22 16:54:40 +0900

    Add files via upload

---
+++ 220322/Homework.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+// while 을 이용해 1~10까지 입력 받는 수를 세다가 사용자가 0의 숫자를 입력하면
+		//입력받은 수가 몇 번이었는지 보여주고, 루프를 종료하는 프로그램을 작성
+		//0
+		//0번 입력 받았습니다.
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+
+		int i =0;
+		while(true)
+		{
+			System.out.println("1-10 사이의 숫자를 입력하세요. 0을 입력하면 종료합니다.");
+			
+			int a = sc.nextInt();
+			i=i+1;
+			
+			if (a>10 || a<0)
+			{
+				System.out.println("잘못입력하였습니다."); // 카운트 해야 하나? 
+			}
+			if (a==0)
+			{
+				break;
+			}
+			
+		}
+		System.out.printf("%d번 입력 받았습니다.",i-1);
+	}
+
+}

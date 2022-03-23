b8d3325e056050a3e08d0df0786d75e2443eeeaa
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-23 12:15:38 +0900

    Add files via upload

---
+++ 220323/Arrays.java
@@ -0,0 +1,77 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//변수선언
+		int[] array;
+		//메모리할당
+		array = new int[5];
+		//값 할당
+		array[0] = 1;
+		array[1] = 3;
+		array[2] = 5;
+		array[3] = 7;
+		array[4] = 9;
+		
+		byte[] arrByte;
+		arrByte = new byte[5];
+		arrByte[0]=1;
+		arrByte[1]=2;
+		arrByte[2]=3;
+		arrByte[3]=4;
+		arrByte[4]=5;
+		
+		char[] arrChar;
+		arrChar = new char[5];
+		arrChar[0]='A';
+		arrChar[1]='B';
+		arrChar[2]='C';
+		arrChar[3]='D';
+		arrChar[4]='E';
+		
+		boolean[] arrBoolean;
+		arrBoolean = new boolean[5];
+		arrBoolean[0] = false;
+		arrBoolean[1] = true;
+		arrBoolean[2] = false;
+		arrBoolean[3] = true;
+		arrBoolean[4] = false;
+		
+		short[] arrShort;
+		arrShort = new short[5];
+		arrShort[0] = 100;
+		arrShort[1] = 200;
+		arrShort[2] = 300;
+		arrShort[3] = 400;
+		arrShort[4] = 500;
+		
+		long[] arrLong;
+		arrLong = new long[5];
+		arrLong[0] = 90L;
+		arrLong[1] = 135L;
+		arrLong[2] = 180L;
+		arrLong[3] = 255L;
+		arrLong[4] = 270L;
+		
+		float[] arrFloat;
+		arrFloat = new float[5];
+		arrFloat[0] = 3.14f;
+		arrFloat[1] = 6.28f;
+		arrFloat[2] = 9.42f;
+		arrFloat[3] = 12.56f;
+		arrFloat[4] = 15.700001f;
+		
+		double[] arrDouble;
+		arrDouble = new double[5];
+		arrDouble[0] = 3.141592;
+		arrDouble[1] = 3.141592;
+		arrDouble[2] = 3.141592;
+		arrDouble[3] = 3.141592;
+		arrDouble[4] = 3.141592;
+		
+		
+	}
+
+}

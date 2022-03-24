f2459bb6fd659414d92add6723e4a0629ddc05be
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-24 14:32:37 +0900

    Add files via upload

--- 220323/Ex24.java
+++ 220323/Ex24.java
@@ -17,7 +17,8 @@ public class Ex24 {
 		{
 			System.out.printf("정수 입력 %d : ",i+1);
 			su[i] = sc.nextInt();
-							
+					
+			//가장 큰 값 구하기
 			if (su[i]>j)
 			{
 				j= su[i];
@@ -27,9 +28,55 @@ public class Ex24 {
 				continue;
 			}
 		}
-		//가장 큰 값 구하기
+		
 		System.out.println(j);
 	
+		
+		//5명의 수학점수를 입력받아 총점과 평균을 구하라
+		
+		int hap=0;
+		int[] st = new int[5];
+		for(int k =0; k<5; k++)
+		{
+			System.out.printf("수학점수 입력 %d : ",k+1);
+			st[k] = sc.nextInt();
+						
+			hap = hap + st[k];
+		}
+		float avg =0;
+		avg = (int)hap /5;
+		
+		System.out.printf("총점 : %d",hap);
+		System.out.printf("평균 : %.1f",avg);
+		
+		
+		//1-10까지 저장된 배열 10개를 저장해두고, 검색할 숫자를 입력하여 찾아라
+		
+		int[] bae = new int[] {1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println("숫자를 입력하세요");
+		int a = sc.nextInt();
+		
+		if (a<0||a>10)
+		{
+			System.out.println("잘못입력하였습니다.");
+		
+		}
+		else
+		{
+			for (int c = 0; c<10; c++)
+			{
+				if (a==bae[c])
+				{
+					System.out.printf("찾았다, %d",a);
+				}
+
+				else
+				{
+					continue;
+				}
+			}
+		}
 		
 		
 	}

47924c52504c449d2b6748dedeeb6afd71232da9
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-30 20:09:55 +0900

    Add files via upload

---
+++ 220330/Ex_9.java
@@ -0,0 +1,19 @@
+
+public class Ex_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[][] arr = {
+				{5,5,5,5,5},
+				{10,10,10},
+				{20,20,20,20},
+				{30,30}
+		};
+		
+		System.out.println(arr.length); //4
+		System.out.println(arr[3].length); //2
+	}
+
+}

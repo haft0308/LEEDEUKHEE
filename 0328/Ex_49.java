07c11d7f5fbfa2646240699fbfaab80fff498bb5
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-30 17:02:44 +0900

    Add files via upload

---
+++ 0328/Ex_49.java
@@ -0,0 +1,17 @@
+
+public class Ex_49 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//배열 arr2의 length? 배열 arr2[4]의 length? ==>5, 7
+		int[][] arr2 = new int[5][7];
+		System.out.println(arr2.length);
+		System.out.println(arr2[4].length);
+		
+		//배열 arr3[2].length? ==> 3
+		int[][] arr3 = new int[8][3];
+		System.out.println(arr3[2].length);
+				
+	}
+
+}

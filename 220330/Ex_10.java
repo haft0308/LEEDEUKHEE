faebac8cbf134b350f8f0f579f93b73d99730714
DEUKHEE <101791490+santoNNrini0078@users.noreply.github.com>
2022-03-31 14:16:25 +0900

    Update Ex_10.java

--- 220330/Ex_10.java
+++ 220330/Ex_10.java
@@ -9,13 +9,13 @@ public class Ex_10 {
 		
 		int a = 2680;
 		int[] b = new int[]{500,100,50,10};
-		int[] d = new int[5];
-		d[0] = 2680;
+		int[] d = new int[4];
+		
 		for(int i=0; i<4; i++)
 		{		
-			int c = d[i]/b[i];
-			d[i+1] = a%b[i];
-			System.out.printf("%d 동전의 갯수는 %d\n",b[i],c);
+			d[i] = a/b[i];
+			a = a%b[i];
+			System.out.printf("%d 동전의 갯수는 %d\n",b[i],d[i]);
 		}
 		
 	}
